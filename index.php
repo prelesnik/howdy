f1bc6ad07edd1861692d62fa1294a565ab2e5b9d
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-09 13:37:24 -0500

    added an index homepage and a composer.json file to configure fat free

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    //turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once "vendor/autoload.php";
+
+    //create an instance of the base class
+    $f3 = Base::instance();
+
+    //turn on fat-free error reporting
+    $f3->set('DEBUG', 3);
+
+    //define a default route
+    $f3->route('GET /', function() {
+        echo "<h1>Howdy!</h1>";
+        //$view = new View;
+        //echo $view->render('views/home-page.html');
+    });
+
+    //run fat-free
+    $f3->run();
